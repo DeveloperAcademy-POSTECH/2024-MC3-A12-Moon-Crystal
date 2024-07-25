f77f23f048a0926f6c6649e5539f3ef07d4fe762
zaehorang <zaehorang2@naver.com>
2024-07-25 18:40:38 +0900

    chore: Update initial project

---
+++ MoonCrystal/MoonCrystal/MoonCrystalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonCrystalApp.swift
+//  MoonCrystal
+//
+//  Created by zaehorang on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonCrystalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
