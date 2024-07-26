a76c9326e8dc2ee6e6f2ad0cdcda386a3bf1f9ab
zaehorang <zaehorang2@naver.com>
2024-07-26 15:56:28 +0900

    feat: Declare LocalNotification struct type

---
+++ MoonCrystal/MoonCrystal/Notification/LocalNotificationType.swift
@@ -0,0 +1,14 @@
+//
+//  LocalNotification.swift
+//  MoonCrystal
+//
+//  Created by zaehorang on 7/26/24.
+//
+
+struct LocalNotification {
+    var identifier: String
+    var title: String
+    var body: String
+    var timeInterval: Double
+    var repeats: Bool
+}
