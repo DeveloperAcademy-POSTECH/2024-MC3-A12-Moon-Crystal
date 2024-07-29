2d8c707a7b4612882ebfae94a770efb5b9250040
zaehorang <zaehorang2@naver.com>
2024-07-29 23:47:42 +0900

    refactor: [#7] Modify model to use SwiftData instead of struct

--- MoonCrystal/MoonCrystal/UserProfile/UserProfile.swift
+++ MoonCrystal/MoonCrystal/UserProfile/UserProfile.swift
@@ -6,34 +6,17 @@
 //
 
 import Foundation
+import SwiftData
 
-struct UserProfile: Codable {
+@Model
+final class UserProfile {
     var favoriteIdol: String
     var nickname: String
     var image: Data
     
-    // struct를 Data타입으로 인코딩함. 리턴값은 AppStorage에 실제로 저장되는 값.
-    func encode() -> Data? {
-        let encoder = JSONEncoder()
-        
-        if let encoded = try? encoder.encode(self) {
-            return encoded
-        } else {
-            print("❌ UserProfile/encode")
-            return nil
-        }
+    init(favoriteIdol: String, nickname: String, image: Data) {
+        self.favoriteIdol = favoriteIdol
+        self.nickname = nickname
+        self.image = image
     }
-    
-    // AppStorage에서 Data값을 가져오면, User 구조체로 다시 변환해 화면에 뿌려줄 수 있음.
-    static func decode(userData: Data) -> UserProfile? {
-        let decoder = JSONDecoder()
-        
-        if let user = try? decoder.decode(UserProfile.self, from: userData) {
-            return user
-        } else {
-            print("❌ UserProfile/decode")
-            return nil
-        }
-    }
-    
 }
