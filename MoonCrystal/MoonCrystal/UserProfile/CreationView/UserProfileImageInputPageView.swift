3c9730c41bd93465450b1d029357b9411ae190d3
zaehorang <zaehorang2@naver.com>
2024-07-30 03:25:35 +0900

    feat: [#7] Add UserProfileImageInputPageView for image input
    - 프로필 생성 완료 및 이미지 입력을 위한 UserProfileImageInputPageView 추가
    - 프로필 생성을 완료하고 뷰를 해제하기 위한 버튼 추가

---
+++ MoonCrystal/MoonCrystal/UserProfile/CreationView/UserProfileImageInputPageView.swift
@@ -0,0 +1,37 @@
+//
+//  UserProfileImageInputPageView.swift
+//  MoonCrystal
+//
+//  Created by zaehorang on 7/30/24.
+//
+
+import SwiftUI
+
+struct UserProfileImageInputPageView: View {
+    @Binding var currentPage: Int
+    @Binding var userProfileData: UserProfileInputModel
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        //TODO: 프로필에게 프로필 이미지 받기
+        
+        Button {
+            //TODO: 프로필 저장 로직 추가하기
+            presentationMode.wrappedValue.dismiss()
+        } label: {
+            Text("완료")
+                .font(.title2)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .padding()
+        }
+    }
+}
+
+#Preview {
+    UserProfileImageInputPageView(currentPage: .constant(3), userProfileData: .constant(UserProfileInputModel(favoriteIdol: "뉴진스", nickname: "뉴뉴뉴", image: nil)))
+}
