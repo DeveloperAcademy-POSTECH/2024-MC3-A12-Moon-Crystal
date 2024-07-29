2d208ddd6970c84d8dc09c24e83e02f54e5d7de8
zaehorang <zaehorang2@naver.com>
2024-07-29 16:22:13 +0900

    feat: [#7] Add mockup image for profile

---
+++ MoonCrystal/MoonCrystal/UserProfile/UserProfileCreationView.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileCreationView.swift
+//  MoonCrystal
+//
+//  Created by zaehorang on 7/29/24.
+//
+
+import SwiftUI
+
+struct UserProfileCreationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfileCreationView()
+}
--- MoonCrystal/MoonCrystal/UserProfile/UserProfileView.swift
+++ MoonCrystal/MoonCrystal/UserProfile/UserProfileView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct UserProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image("newjeans")
+        
+        
     }
 }
 
