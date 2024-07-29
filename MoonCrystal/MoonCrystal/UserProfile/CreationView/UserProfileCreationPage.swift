56559b2425b71b92a24c8c01475fc5d5ddaf003e
zaehorang <zaehorang2@naver.com>
2024-07-30 03:16:00 +0900

    feat: Add UserProfileCreationPage enum with titles and descriptions

---
+++ MoonCrystal/MoonCrystal/UserProfile/CreationView/UserProfileCreationPage.swift
@@ -0,0 +1,34 @@
+//
+//  UserProfileCreationPageText.swift
+//  MoonCrystal
+//
+//  Created by zaehorang on 7/30/24.
+//
+
+enum UserProfileCreationPage: Int {
+    case favoriteIdol = 1
+    case nickname
+    case idolImage
+    
+    var title: String {
+        switch self {
+        case .favoriteIdol:
+            return "누구를 위해\n용량을 정리할까요?"
+        case .nickname:
+            return "어떻게\n불러드릴까요?"
+        case .idolImage:
+            return "좋아하는 최애 사진을\n업로드 해주세요"
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .favoriteIdol:
+            return "좋아하는 그룹이름 또는 최애를 적어도 좋아요"
+        case .nickname:
+            return "팬덤 이름 또는 원하는 닉네임을 적어도 좋아요"
+        case .idolImage:
+            return "홈 화면 프로필 아이콘, 팁에 사용되어요"
+        }
+    }
+}
