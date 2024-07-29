0b6b3840e1254377458130760fd5a32cc7fe5af2
zaehorang <zaehorang2@naver.com>
2024-07-30 03:12:06 +0900

    feat: Add basic UserProfileView structure

--- MoonCrystal/MoonCrystal/UserProfile/UserProfileView.swift
+++ MoonCrystal/MoonCrystal/UserProfile/UserProfileView.swift
@@ -8,10 +8,32 @@
 import SwiftUI
 
 struct UserProfileView: View {
+    // TODO: 로컬에서 프로필 저장 유무 받아오기
+    // 이전 뷰에서도 프로필을 보여주니까 해당 값의 유무에 따라 나누면 될 듯
+    var isUserProfileSaved = false
+    
     var body: some View {
-        Image("newjeans")
-        
-        
+        if isUserProfileSaved {
+            // TODO: 프로필 뷰 그리기
+            Image("newjeans")
+                .navigationTitle("프로필")
+        } else {
+            VStack(spacing: 24) {
+                // TODO: 텍스트 UI 수정
+                Text("아직 작성된 프로필이 없어요\n프로필을 작성해주세요")
+                    .font(.system(size: 18))
+                    .fontWeight(.regular)
+                    .multilineTextAlignment(.center)
+                    
+                NavigationLink {
+//                    UserProfileCreationView()
+                } label: {
+                    // TODO: 버튼 이미지 수정
+                    Image("ProfileAddButton")
+                }
+            }
+            .navigationTitle("프로필")
+        }
     }
 }
 
