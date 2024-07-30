6a04d8c8565f395b97d1eaf01b43bff6c5919472
Seon-U <seonu.kim.kr@gmail.com>
2024-07-30 13:49:19 +0900

    style: [#3] change annotation and spacing
    -변수명 뒤에 타입 선언할 때 붙여쓰기로 수정
    -생성시 기본 주석을 파일명에 맞춰 수정

--- MoonCrystal/MoonCrystal/VideoFormatCapacity.swift
+++ MoonCrystal/MoonCrystal/VideoFormatCapacity.swift
@@ -1,5 +1,5 @@
 //
-//  V.swift
+//  VideoFormatCapacity.swift
 //  MoonCrystal
 //
 //  Created by seonu kim on 7/29/24.
@@ -8,18 +8,18 @@
 import Foundation
 
 /// 비디오 포멧별 1분당  데이터를 byte단위로 정리한 enum입니다.
-enum VideoFormatCapacity : Int, CaseIterable {
+enum VideoFormatCapacity: Int, CaseIterable {
     case uhd4k60fps = 461_373_440
     case uhd4k30fps = 199_229_440
     case hd1080p60fps = 104_857_600
     case defaultQuality = 68_157_440
     
-    var detailInfo : String {
+    var detailInfo: String {
         switch self {
-        case .uhd4k60fps : return "4K - 60fps"
-        case .uhd4k30fps : return "4K - 24fps"
-        case .hd1080p60fps : return "1080p HD - 60fps"
-        case .defaultQuality : return "1080p HD - 30fps"
+        case .uhd4k60fps: return "4K - 60fps"
+        case .uhd4k30fps: return "4K - 24fps"
+        case .hd1080p60fps: return "1080p HD - 60fps"
+        case .defaultQuality: return "1080p HD - 30fps"
         }
     }
 }
