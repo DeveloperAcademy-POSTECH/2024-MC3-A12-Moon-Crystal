34a20cd15b1f83f2a8d7365567ec3a5ffcbcb873
zaehorang <zaehorang2@naver.com>
2024-07-30 22:26:02 +0900

    feat: [#7] Add UserProfileImageInputPageView for image input
    - 사진 라이브러리에서 이미지를 선택할 수 있도록 PhotosPicker 구현
    - 이미지가 선택될 때까지 완료 버튼 비활성화
    - 포토 피커 버튼에 플레이스홀더 이미지 추가

--- MoonCrystal/MoonCrystal/UserProfile/CreationView/UserProfileImageInputPageView.swift
+++ MoonCrystal/MoonCrystal/UserProfile/CreationView/UserProfileImageInputPageView.swift
@@ -5,6 +5,7 @@
 //  Created by zaehorang on 7/30/24.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct UserProfileImageInputPageView: View {
@@ -13,24 +14,71 @@ struct UserProfileImageInputPageView: View {
     
     @Environment(\.presentationMode) var presentationMode
     
+    @State var uiImage: UIImage?
+    @State var imageSelection: PhotosPickerItem?
+    
+    var page: UserProfileCreationPage
+    
     var body: some View {
-        //TODO: 프로필에게 프로필 이미지 받기
-        
-        Button {
-            //TODO: 프로필 저장 로직 추가하기
-            presentationMode.wrappedValue.dismiss()
-        } label: {
-            Text("완료")
-                .font(.title2)
-                .padding()
-                .frame(maxWidth: .infinity)
-                .background(.black)
-                .foregroundColor(.white)
-                .cornerRadius(10)
+        VStack(spacing: 40) {
+            VStack(spacing: 42) {
+                HStack {
+                    VStack(alignment: .leading, spacing: 6) {
+                        Text(page.title)
+                            .font(.system(size: 28))
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.leading)
+                        
+                        Text(page.description)
+                            .font(.system(size: 16))
+                            .fontWeight(.regular)
+                    }
+                    Spacer()
+                }
+                photoPickerButton
+                    .padding(.horizontal, 23)
+            }
+            
+            Spacer()
+            
+            Button {
+                //TODO: 프로필 저장 로직 추가하기
+                presentationMode.wrappedValue.dismiss()
+            } label: {
+                Text("완료하기")
+                    .font(.title2)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(uiImage == nil ? .gray : .black)
+                    .foregroundColor(.white)
+                    .cornerRadius(12)
+            }
+            .disabled(uiImage == nil)
         }
+    }
+    
+    private var photoPickerButton: some View {
+        PhotosPicker(
+            selection: $imageSelection,
+            matching: .images,
+            photoLibrary: .shared()) {
+                Image(uiImage: uiImage ?? UIImage(named: "ProfileImagePlaceholder")!)
+                    .resizable()
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+            }
+            .onChange(of: imageSelection) {
+                Task { @MainActor in
+                    if let data = try? await imageSelection?.loadTransferable(type: Data.self) {
+                        userProfileData.imageData = data
+                        uiImage = UIImage(data: data)
+                        return
+                    }
+                }
+            }
     }
 }
 
 #Preview {
-    UserProfileImageInputPageView(currentPage: .constant(3), userProfileData: .constant(UserProfileInputModel(favoriteIdol: "뉴진스", nickname: "뉴뉴뉴", image: nil)))
+    UserProfileImageInputPageView(currentPage: .constant(3), userProfileData: .constant(UserProfileInputModel(favoriteIdol: "뉴진스", nickname: "뉴뉴뉴", imageData: nil)), page: .idolImage)
 }
