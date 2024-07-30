62e8f5c6b214bff97a5afc8c91ad3ce71b640911
Seon-U <seonu.kim.kr@gmail.com>
2024-07-30 13:52:00 +0900

    Style: [#3] delete unused framework
    -Foundation을 선언하지 않아도 되는 화면에 있던 Foundation 제거

--- MoonCrystal/MoonCrystal/MediaCapacityConverter.swift
+++ MoonCrystal/MoonCrystal/MediaCapacityConverter.swift
@@ -5,8 +5,6 @@
 //  Created by seonu kim on 7/27/24.
 //
 
-import Foundation
- 
 /// 남은 잔여 용량을 비디오 촬영시간 또는 촬영 가능 사진 수로 반환하는 함수들의 class입니다.
 class MediaCapacityConverter {
     static func capacityToTime(capacity: Int, format: VideoFormatCapacity) -> Int {
