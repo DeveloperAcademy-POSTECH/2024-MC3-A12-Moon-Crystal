fdec826123916de46c668f7a5c68fae397120864
Seon-U <seonu.kim.kr@gmail.com>
2024-07-30 14:50:24 +0900

    Style: [#9] change function name and value name
    -totalStorage -> totalCapacity로 바꿨습니다
    -checkDeviceStorage를 변수명과 일치하며 updateTotalCapacity로 바꿨습니다.

--- MoonCrystal/MoonCrystal/CapacityCalculator.swift
+++ MoonCrystal/MoonCrystal/CapacityCalculator.swift
@@ -8,41 +8,41 @@
 import Foundation
 
 class CapacityCalculator: ObservableObject {
-    @Published var totalStorage = 0
-    @Published var freeStorage = 0
+    @Published var totalCapacity = 0
+    @Published var freeCapacity = 0
     
     init() {
-        checkDeviceStorage()
-        checkLeftStorage()
+        updateTotalCapacity()
+        updateFreeCapacity()
     }
     
-    func checkDeviceStorage() {
+    func updateTotalCapacity() {
         let fileURL = URL(filePath: "/")
         do {
             let values = try fileURL.resourceValues(forKeys: [.volumeTotalCapacityKey])
             if let capacity = values.volumeTotalCapacity {
-                self.totalStorage = capacity
+                self.totalCapacity = capacity
             } else {
                 print("❌ StorageManager/checkDeviceStorage Capacity is unavailable")
             }
         } catch {
             print("❌ StorageManager/checkDeviceStorage Error retrieving capacity: \(error.localizedDescription)")
         }
-        self.totalStorage = 0
+        self.totalCapacity = 0
     }
     
-    func checkLeftStorage() {
+    func updateFreeCapacity() {
         let fileURL = URL(filePath: "/")
         do {
             let values = try fileURL.resourceValues(forKeys: [.volumeAvailableCapacityForImportantUsageKey])
             if let capacity = values.volumeAvailableCapacityForImportantUsage {
-                self.freeStorage = Int(capacity)
+                self.freeCapacity = Int(capacity)
             } else {
                 print("❌ StorageManager/checkLeftStorage Capacity is unavailable")
             }
         } catch {
             print("❌ StorageManager/checkLeftStorage Error retrieving capacity: \(error.localizedDescription)")
         }
-        self.freeStorage = 0
+        self.freeCapacity = 0
     }
 }
