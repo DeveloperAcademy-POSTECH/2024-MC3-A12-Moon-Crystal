f7ff56bda3160459cc9a232e65537fddbecd7b66
zaehorang <zaehorang2@naver.com>
2024-07-31 02:45:26 +0900

    refactor: [#7] Simplify PhotoPickerButton

--- MoonCrystal/MoonCrystal/PhotoPickerButton.swift
+++ MoonCrystal/MoonCrystal/PhotoPickerButton.swift
@@ -9,8 +9,7 @@ import PhotosUI
 import SwiftUI
 
 struct PhotoPickerButton: View {
-    @Binding var uiImage: UIImage?
-    @Binding var imageSelection: PhotosPickerItem?
+    @State var imageSelection: PhotosPickerItem?
     @Binding var userProfileData: UserProfileInputModel
     
     var body: some View {
@@ -18,16 +17,21 @@ struct PhotoPickerButton: View {
             selection: $imageSelection,
             matching: .images,
             photoLibrary: .shared()) {
-                Image(uiImage: uiImage ?? UIImage(named: "ProfileImagePlaceholder")!)
-                    .resizable()
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                if let imageData = userProfileData.imageData, let uiImage = UIImage(data: imageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                } else {
+                    Image("ProfileImagePlaceholder")
+                        .resizable()
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                }
             }
             .onChange(of: imageSelection) {
                 Task { @MainActor in
                     do {
                         if let data = try await imageSelection?.loadTransferable(type: Data.self) {
                             userProfileData.imageData = data
-                            uiImage = UIImage(data: data)
                         }
                     } catch {
                         print("❌ PhotoPickerButton: \(error.localizedDescription)")
