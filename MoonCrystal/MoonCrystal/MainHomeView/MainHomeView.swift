240fd1b54b2f51bf6fbbb755efa968548c5aa2b4
Kkugy <tjdrnrdla@hanmail.net>
2024-07-31 15:41:19 +0900

    chore: [#10] Add ColorAsset and Change code style

--- MoonCrystal/MoonCrystal/MainHomeView/MainHomeView.swift
+++ MoonCrystal/MoonCrystal/MainHomeView/MainHomeView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct MainHomeView: View {
-    @State var availableTime = "0h 36m"
+    @State var availableTimeText = "0h 36m"
     @State var name = "최애"
     @State private var progress: Float = 0.0
     @State var uiImageName: UIImage? = nil
@@ -21,7 +21,7 @@ struct MainHomeView: View {
                     NavigationLink {
                         // TODO: UserProfileView 연결
                     } label: {
-                        ProfileViewButton
+                        profileViewButton
                             .padding(.top, 74)
                             .padding(.trailing)
                     }
@@ -29,81 +29,77 @@ struct MainHomeView: View {
                 NavigationLink {
                     // TODO: 그동안 삭제된 양 확인하는 뷰 연결
                 } label: {
-                    DeletedStorageViewButton
+                    deletedStorageViewButton
                         .padding(.top, 28)
                         .padding(.horizontal)
                 }
-                AvailableTime
+                availableTime
                     .padding(.leading, 40)
                 Divider()
-                    .foregroundStyle(Color("gray200"))
+                    .foregroundStyle(Color.gray200)
                     .padding(.horizontal, 40)
                     .padding(.top, 33)
-                CurrentCapacityTitle
+                currentCapacityTitle
                     .padding(.horizontal, 40)
                     .padding(.top, 39)
-                ZStack{
+                ZStack {
                     ProgressHalfCircleView(progress: self.$progress)
                         .padding(.top, 32)
                         .padding(.horizontal, 50)
                     
-                    NavigationLink{
+                    NavigationLink {
                     } label: {
-                        CleanUpViewButton
-
+                        cleanUpViewButton
                     }
                     .padding(.top, 260)
                     .padding(.horizontal)
                 }
                 Spacer()
             }
-            .background(Color("gray50"))
+            .background(Color.gray50)
             .edgesIgnoringSafeArea(.all)
         }
         .tint(.black)
         .onAppear {
             // TODO: 데이터 fetch
-            availableTime = "1h 30m"
+            availableTimeText = "1h 30m"
             name = "최애"
             progress = 0.8
             //uiImageName =
         }
     }
     
-    var ProfileViewButton: some View{
-        ZStack{
+    var profileViewButton: some View {
+        ZStack {
             if uiImageName == nil {
                 Circle()
                     .frame(width: 52, height: 52)
                     .foregroundStyle(.white)
-                    .overlay(Circle().stroke(Color("Stroke"), lineWidth: 0.5))
-                
+                    .overlay(Circle().stroke(Color.gray200, lineWidth: 0.5))
                 Image(systemName: "person")
                     .fontWeight(.bold)
-                    .foregroundStyle(Color("gray700"))
+                    .foregroundStyle(Color.gray700)
                     .frame(width: 24, height: 24)
-                
             } else {
                 Image(uiImage: uiImageName!)
                     .resizable()
                     .scaledToFill()
                     .frame(width: 52, height: 52)
                     .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                    .overlay(Circle().stroke(Color("Stroke"), lineWidth: 0.5))
-                
+                    .overlay(Circle().stroke(Color.gray200, lineWidth: 0.5))
             }
         }
     }
     
-    var DeletedStorageViewButton: some View {
-        ZStack{
+    var deletedStorageViewButton: some View {
+        ZStack {
             RoundedRectangle(cornerRadius: 32)
                 .frame(height: 64)
                 .foregroundStyle(.white)
-            HStack{
+            HStack {
                 Image(systemName: "heart.fill")
                     .frame(width: 18, height: 18)
-                    .foregroundStyle(Color("pink200"))
+                    .foregroundStyle(Color.pink200)
                     .padding(.trailing, 12)
                 Text("\(name)\(MainHomeViewComponent.deletedStorage.title)")
                     .font(.system(size: 15))
@@ -115,36 +111,35 @@ struct MainHomeView: View {
         }
     }
     
-    var AvailableTime: some View {
-        HStack{
+    var availableTime: some View {
+        HStack {
             VStack(alignment: .leading, spacing: 0.0) {
                 Text(MainHomeViewComponent.availableTime.title)
                     .font(.system(size: 15, weight: .semibold))
-                    .foregroundStyle(Color("gray700"))
+                    .foregroundStyle(Color.gray700)
                     .padding(.top, 38)
-                
-                Text("\(availableTime)")
+                Text("\(availableTimeText)")
                     .font(.system(size: 48, weight: .semibold))
-                    .foregroundStyle(Color("gray900"))
+                    .foregroundStyle(Color.gray900)
                     .padding(.top, 16)
             }
             Spacer()
         }
     }
     
-    var CurrentCapacityTitle: some View {
-        HStack{
+    var currentCapacityTitle: some View {
+        HStack {
             Text(MainHomeViewComponent.currentCapacity.title)
                 .font(.system(size: 15, weight: .semibold))
-                .foregroundStyle(Color("gray700"))
+                .foregroundStyle(Color.gray700)
             Spacer()
         }
     }
     
-    var CleanUpViewButton: some View {
+    var cleanUpViewButton: some View {
         RoundedRectangle(cornerRadius: 12)
             .frame(height: 68)
-            .foregroundStyle(Color("gray900"))
+            .foregroundStyle(Color.gray900)
             .overlay(
                 Text(MainHomeViewComponent.CleanUpButton.title)
                     .foregroundStyle(.white))
@@ -155,6 +150,3 @@ struct MainHomeView: View {
     MainHomeView()
 }
 
-#Preview {
-    MainHomeView()
-}
