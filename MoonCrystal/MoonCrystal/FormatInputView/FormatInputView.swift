57f9575717408bf08b7af2f7a633d1ac2e202e78
Kkugy <tjdrnrdla@hanmail.net>
2024-07-31 20:52:25 +0900

    Style: [#14] Code separation

--- MoonCrystal/MoonCrystal/FormatInputView/FormatInputView.swift
+++ MoonCrystal/MoonCrystal/FormatInputView/FormatInputView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct FormatInputView: View {
     @State var selectedType: VideoFormatCapacity = .defaultQuality
+    
     var body: some View {
         VStack(spacing: 0) {
             HStack {
@@ -17,78 +18,31 @@ struct FormatInputView: View {
                 Spacer()
             }
             HStack() {
-                Button(action: {
-                    selectedType = .defaultQuality
-                }, label: {
-                    RoundedRectangle(cornerRadius: 12)
-                        .frame(height: 60)
-                        .foregroundStyle(.white)
-                        .overlay(
-                            Text("기본 화질")
-                                .foregroundStyle(selectedType == .defaultQuality ? Color.pink300 : .black))
-                })
-                .frame(height: 60)
-                .padding(.trailing, 9)
-                Button(action: {
-                    selectedType = .hd1080p60fps
-                }, label: {
-                    RoundedRectangle(cornerRadius: 12)
-                        .frame(height: 60)
-                        .foregroundStyle(.white)
-                        .overlay(
-                            Text("고화질")
-                                .foregroundStyle(selectedType != .defaultQuality ? Color.pink300 : .black))
-                })
-                .frame(height: 60)
+                formatButton(buttonName: "기본 화면", type: .defaultQuality)
+                    .padding(.trailing, 9)
+                formatButton(buttonName: "고화질", type: .hd1080p60fps)
             }
             .padding(.top, 68)
             
-            HStack {
+            HStack() {
                 if selectedType != .defaultQuality {
-                    VStack {
-                        ForEach(VideoFormatCapacity.allCases, id: \.self) { format in
-                            if format != .defaultQuality {
-                                HStack {
-                                    Button(action: {
-                                        selectedType = format
-                                    }, label: {
-                                        HStack {
-                                            Circle()
-                                                .fill(selectedType == format ? Color.pink300 : Color.white)
-                                                .frame(width: 12, height: 12)
-                                                .overlay(
-                                                    Circle()
-                                                        .stroke(selectedType == format ? Color.pink300 : Color.gray, lineWidth: 1)
-                                                )
-                                            Text(format.detailInfo)
-                                                .foregroundColor(.black)
-                                                .font(.system(size: 16, weight: selectedType == format ? .bold : .regular))
-                                        }
-                                    })
-                                    Spacer()
-                                }
-                                .padding(.vertical, 4)
-                                .padding(.leading, 34)
-                            }
-                        }
-                    }
+                    highFormatRadioButtons
+                        .padding(.top, 10)
                 }
             }
             .frame(height: 260)
-            .background(.white)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
             .padding(.top, 34)
-
-            Button(action: {
-                
-            }, label: {
+            
+            NavigationLink {
+                // 여기서 selectedType 가지고 InputView로 넘어가면 될 것 같습니다
+            } label: {
                 RoundedRectangle(cornerRadius: 12)
                     .frame(height: 68)
                     .foregroundStyle(Color.gray900)
                     .overlay(
-                        Text("고화질로 촬영할래요")
+                        Text("\(selectedType ==  .defaultQuality ? "기본 화질" : "고화질")로 촬영할래요")
                             .foregroundStyle(.white))
-            })
+            }
             .padding(.top, 42)
             Spacer()
         }
@@ -96,6 +50,39 @@ struct FormatInputView: View {
         .padding(.horizontal)
         .ignoresSafeArea()
         .background(Color.gray50)
+    }
+    
+    func formatButton(buttonName: String, type: VideoFormatCapacity) -> some View {
+        Button(action: {
+            selectedType = type
+        }) {
+            RoundedRectangle(cornerRadius: 12)
+                .frame(height: 60)
+                .foregroundStyle(.white)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12).stroke(selectedType == type ? Color.pink300 : .black)
+                )
+                .overlay(
+                    Text(buttonName)
+                        .font(.system(size: 16, weight: selectedType == type ? .semibold : .regular))
+                        .foregroundStyle(selectedType == type ? Color.pink300 : .black)
+                )
+        }
+    }
+    
+    var highFormatRadioButtons: some View {
+        VStack(spacing: 0) {
+            ForEach(VideoFormatCapacity.allCases, id: \.self) { format in
+                if format != .defaultQuality {
+                    highFormatRadioButton(selectedType: $selectedType, format: format)
+                        .padding(.top, 32)
+                        .padding(.leading, 34)
+                }
+            }
+            Spacer()
+        }
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
     }
 }
 
---
+++ MoonCrystal/MoonCrystal/FormatInputView/highFormatRadioButton.swift
@@ -0,0 +1,37 @@
+//
+//  highFormatRadioButton.swift
+//  MoonCrystal
+//
+//  Created by sungkug_apple_developer_ac on 7/31/24.
+//
+
+import SwiftUI
+
+struct highFormatRadioButton: View {
+    @Binding var selectedType: VideoFormatCapacity
+    var format: VideoFormatCapacity
+
+    var body: some View {
+        HStack {
+            Button(action: {
+                selectedType = format
+            }, label: {
+                HStack {
+                    Circle()
+                        .stroke(selectedType == format ? Color.pink300 : Color.gray, lineWidth: 1)
+                        .frame(width: 18, height: 18)
+                        .overlay(
+                            Circle()
+                                .fill(selectedType == format ? Color.pink300 : Color.white)
+                                .frame(width: 12, height: 12)
+                        )
+                    Text(format.detailInfo)
+                        .foregroundColor(.black)
+                        .font(.system(size: 16, weight: selectedType == format ? .bold : .regular))
+                }
+            })
+            Spacer()
+        }
+    }
+}
+
