2c997f6a369185b4fd5ce6b2492329ffa6ccf6de
Kkugy <tjdrnrdla@hanmail.net>
2024-08-02 00:24:50 +0900

    feat: [#23] Add EndCleanUpView

---
+++ MoonCrystal/MoonCrystal/DeletedTotalCapacityView/EndCleanUpView.swift
@@ -0,0 +1,55 @@
+//
+//  EndCleanUpView.swift
+//  MoonCrystal
+//
+//  Created by sungkug_apple_developer_ac on 8/2/24.
+//
+
+import SwiftUI
+
+struct EndCleanUpView: View {
+    var userProfile: UserProfile?
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Text("정리가 종료됐어요!")
+                    .font(.system(size: 24, weight: .semibold))
+                Image("twinkle")
+                    .resizable()
+                    .frame(width: 34, height: 29)
+                    .offset(y: -6)
+                Spacer()
+            }
+            .padding(.top, 116)
+
+            HStack {
+                Text("포토카드레 당신의 마음을 담았어요")
+                    .font(.system(size: 16))
+                Spacer()
+            }
+            .padding(.top, 8)
+            
+            MyFavoriteIdolInfoView(userProfile: userProfile)
+                .padding(.top, 57)
+            Button{} label: {
+                RoundedRectangle(cornerRadius: 12)
+                    .frame(height: 68)
+                    .foregroundStyle(Color.gray900)
+                    .overlay(
+                        Text(MainHomeViewComponent.CleanUpButton.title)
+                            .foregroundStyle(.white)
+                    )
+                    .padding(.top, 65)
+            }
+            Spacer()
+        }
+        .padding(.horizontal)
+        .background(Color.gray50)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+#Preview {
+    EndCleanUpView()
+}
