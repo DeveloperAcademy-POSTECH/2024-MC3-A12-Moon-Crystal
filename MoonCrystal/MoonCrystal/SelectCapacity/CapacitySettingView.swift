6c7e03edb3536f8436cd836f0971d6dec6e3cb81
Seon-U <seonu.kim.kr@gmail.com>
2024-08-01 23:11:26 +0900

    feat: [#20] Add TipView
    -입력화면에서 입력 기준을 제공하는 TipView를 생성하고 추가함

--- MoonCrystal/MoonCrystal/SelectCapacity/CapacitySettingView.swift
+++ MoonCrystal/MoonCrystal/SelectCapacity/CapacitySettingView.swift
@@ -168,6 +168,18 @@ struct CapacitySettingView: View {
     }
 }
 
+struct TestView : View {
+    var body : some View {
+        NavigationStack {
+            NavigationLink {
+                CapacitySettingView()
+            } label: {
+                Text("dnskdnla")
+            }
+        }
+    }
+}
 #Preview {
-    CapacitySettingView()
+    TestView()
+//    CapacitySettingView()
 }
