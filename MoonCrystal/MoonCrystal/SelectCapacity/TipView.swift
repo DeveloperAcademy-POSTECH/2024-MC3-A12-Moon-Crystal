853a2deb9e34c692448d37444687d7f0b4ab5d30
Seon-U <seonu.kim.kr@gmail.com>
2024-08-01 23:11:26 +0900

    feat: [#20] Add TipView
    -입력화면에서 입력 기준을 제공하는 TipView를 생성하고 추가함

--- MoonCrystal/MoonCrystal/SelectCapacity/TipView.swift
+++ MoonCrystal/MoonCrystal/SelectCapacity/TipView.swift
@@ -9,31 +9,111 @@ import SwiftUI
 
 ///미완성된 파일입니다.
 struct TipView: View {
+    @Environment(\.dismiss) var dismiss
     let title = "정리를 시작하기 전,\n얼마나 삭제할 지 감이 안 온다면?"
+    
     var body: some View {
-        VStack(alignment: .leading, spacing:0) {
-            HStack(alignment: .top, spacing: 0) {
-                Text("Tip")
-                    .padding(.leading, 20)
-                Spacer()
-                Image(systemName: "xmark")
-                    .font(.system(size: 16))
-                    .padding(.trailing, 27)
+        ZStack {
+            Color(red: 0.96, green: 0.96, blue: 0.96)
+            VStack(alignment: .leading, spacing: 0) {
+                HStack(alignment: .top, spacing: 0) {
+                    Text("Tip")
+                        .font(.system(size: 20, weight: .semibold))
+                        .padding(.leading, 20)
+                    Spacer()
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "xmark")
+                            .font(.system(size: 16, weight: .semibold))
+                            .frame(height: 30)
+                            .padding(.trailing, 27)
+                            .foregroundStyle(.gray700)
+                    }
+                }
+                Text(title)
+                    .font(.system(size: 24, weight: .semibold))
+                    .padding(.leading, 22)
+                    .padding(.top, 23)
+                    .foregroundColor(.gray700)
+                    .fixedSize(horizontal: true, vertical: true)
+                ZStack (alignment: .topLeading) {
+                    VStack(alignment: .leading, spacing: 16) {
+                        infoMessage(informationText: "사진 약 1700장을 삭제")
+                        infoMessage(informationText: "5분 분량의 영상을 12개 삭제")
+                        infoMessage(informationText: "1시간 분량의 영상을 삭제")
+                    }
+                    .padding(.trailing, 20)
+                    
+                    //나중에 이미지 연결할 때 이미지데이터 넣어주세요.
+                    circleIcon(image: nil)
+                        .frame(height: 72)
+                        .offset(x: -48, y: -32)
+                    
+                }
+                .padding(.leading, 72)
+                .padding(.top, 66)
             }
-            Text(title)
-                .font(.system(
-                    size: 24,
-                    weight: .semibold))
-                .padding(.leading, 22)
-                .padding(.top, 23)
-                .foregroundColor(Color.gray700)
-                .fixedSize(horizontal: true, vertical: true)
         }
-        Rectangle()
-            .cornerRadius(22, corners: [.topRight, .bottomRight])
-            .cornerRadius(20, corners: .bottomLeft)
-            .cornerRadius(4, corners: .topLeft)
-            .frame(width: 200, height: 100)
+        .ignoresSafeArea()
+    }
+    
+    private func infoMessage(informationText : String) -> some View {
+        let GBInfoText = "5GB를 확보하려면"
+        let endingText = "해야해요"
+        
+        return HStack {
+            VStack(alignment: .leading, spacing: 0) {
+                Text(GBInfoText)
+                    .font(.system(size: 16))
+                    .foregroundStyle(.gray800)
+                HStack(spacing: 0) {
+                    Text(informationText)
+                        .font(.system(size: 16, weight: .bold))
+                        .foregroundStyle(.pink300)
+                    Text(endingText)
+                        .font(.system(size: 16))
+                        .foregroundStyle(.gray800)
+                }
+            }
+            Spacer()
+        }
+        .padding(.horizontal, 24)
+        .padding(.vertical, 15)
+        .background(Color.white)
+        .clipShape(
+            .rect(
+                topLeadingRadius: 4,
+                bottomLeadingRadius: 20,
+                bottomTrailingRadius: 22,
+                topTrailingRadius: 22
+            )
+        )
+    }
+    
+    private func circleIcon(image : Data?) -> some View {
+        ZStack {
+            if let uiimage = UIImage(data: image ?? Data()) {
+                Image(uiImage: uiimage)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .frame(width: 63)
+            } else {
+                Circle()
+                    .frame(height: 72)
+                    .foregroundStyle(.white)
+                    .overlay {
+                        Image(systemName: "person")
+                            .font(.system(size: 28, weight: .bold))
+                            .foregroundStyle(.gray700)
+                    }
+                Circle()
+                    .stroke(.pink300, lineWidth: 2)
+                    .frame(height: 72)
+                    .padding(1)
+            }
+        }
     }
 }
 
