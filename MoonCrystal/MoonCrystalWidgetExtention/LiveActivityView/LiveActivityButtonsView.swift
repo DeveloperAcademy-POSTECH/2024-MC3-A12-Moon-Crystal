58772d55c0b3503dc0ecc535a0a78ee0bfd8717a
Kkugy <tjdrnrdla@hanmail.net>
2024-08-02 11:50:58 +0900

    feat: [#5] ReDesign dynamicIslandUI

--- MoonCrystal/MoonCrystalWidgetExtention/LiveActivityView/LiveActivityButtonsView.swift
+++ MoonCrystal/MoonCrystalWidgetExtention/LiveActivityView/LiveActivityButtonsView.swift
@@ -14,41 +14,39 @@ struct LiveActivityButtonsView: View {
         HStack {
             if !isLoading {
                 Button(intent: CompleteTaskAppIntent(), label: {
-                    Label("종료", image:"")
+                    Text("종료")
+                        .font(.system(size: 14, weight: .regular))
                 })
                 .buttonStyle(.plain)
-                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,minHeight:40,maxHeight:40)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/,minHeight:44,maxHeight:44)
                 .background(.gray800)
-                .clipShape(RoundedRectangle(cornerRadius: 12))
+                .clipShape(RoundedRectangle(cornerRadius: 16))
                 .foregroundStyle(Color.white)
             } else {
                 loadingButton
             }
             
             Button(intent: UpdateTaskAppIntent(), label: {
-                Label("새로고침", image:"")
+                Text("업데이트")
+                    .font(.system(size: 14, weight: .regular))
             })
             .buttonStyle(.plain)
             .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight:44, maxHeight:44)
             .background(.pink300)
-            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .clipShape(RoundedRectangle(cornerRadius: 16))
             .foregroundStyle(Color.white)
         }
     }
     
     var loadingButton: some View {
-        RoundedRectangle(cornerRadius: 12)
+        RoundedRectangle(cornerRadius: 16)
             .frame(width: .infinity,height: 40)
             .foregroundStyle(.gray800)
             .overlay(content: {
                 ProgressView(timerInterval: Date()...Date(timeIntervalSinceNow: 2), countsDown: false, label: {}, currentValueLabel: {})
-                .frame(width: 26,height: 26)
-                .progressViewStyle(.circular)
-                .foregroundStyle(.white)
+                    .frame(width: 26,height: 26)
+                    .progressViewStyle(.circular)
+                    .foregroundStyle(.white)
             })
     }
-}
-
-#Preview {
-    LiveActivityButtonsView()
 }
