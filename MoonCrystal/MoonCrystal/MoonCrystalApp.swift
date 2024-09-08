6a170a4098bd7015cb89b69af77665116e686c53
Seon-U <seonu.kim.kr@gmail.com>
2024-09-09 06:08:47 +0900

    feat: [#101] Add launch screen
    - 런치스크린용 에셋을 추가함
    - 런치스크린을 info.plist에 추가함
    - 스플래시 뷰 화면을 위해 앱에 초기 지연을 설정함

--- MoonCrystal/MoonCrystal/MoonCrystalApp.swift
+++ MoonCrystal/MoonCrystal/MoonCrystalApp.swift
@@ -11,6 +11,11 @@ import SwiftUI
 @main
 struct MoonCrystalApp: App {
     
+    /// 스플래시 뷰 설정을 위한 시간지연
+    init() {
+        Thread.sleep(forTimeInterval: 0.5)
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
