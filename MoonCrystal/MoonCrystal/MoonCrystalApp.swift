5593f3e21547d81d9da7ba359f4eef3458369743
zaehorang <zaehorang2@naver.com>
2024-10-03 21:24:06 +0900

    feat: [#115] Add ATT tracking authorization request in AppDelegate

--- MoonCrystal/MoonCrystal/MoonCrystalApp.swift
+++ MoonCrystal/MoonCrystal/MoonCrystalApp.swift
@@ -5,6 +5,8 @@
 //  Created by zaehorang on 7/25/24.
 //
 
+import AdSupport
+import AppTrackingTransparency
 import FirebaseCore
 import SwiftData
 import SwiftUI
@@ -31,8 +33,27 @@ struct MoonCrystalApp: App {
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // Firebase 초기화
         FirebaseApp.configure()
         
+        // 앱 추적 권한 요청을 비동기적으로 처리
+        Task {
+            await requestTrackingAuthorization()
+        }
         return true
+    }
+    
+    /// 비동기 추적 권한 요청 함수
+    private func requestTrackingAuthorization() async {
+        // 1초 지연 후 추적 권한 요청
+        do {
+            try await Task.sleep(nanoseconds: 1_000_000_000) // 1초 = 1,000,000,000 나노초
+        } catch {
+            print(error)
+        }
+        
+        if ATTrackingManager.trackingAuthorizationStatus == .notDetermined {
+            let _ = await ATTrackingManager.requestTrackingAuthorization()
+        }
     }
 }
